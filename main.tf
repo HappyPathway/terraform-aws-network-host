404aa8e6d083aaaca414ece761160d6d20ce506c
Dave Arnold <darnold@hashicorp.com>
2018-06-29 17:26:15 -0700

    fixing egress

--- main.tf
+++ main.tf
@@ -37,6 +37,14 @@ resource "aws_security_group" "ssh" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+    prefix_list_ids = ["pl-12c4e678"]
+  }
 }
 
 resource "aws_instance" "web" {
