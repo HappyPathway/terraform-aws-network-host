0dd33c4243b47a4c4de5716a94e484c7b231b857
David Arnold <dja+djaboxx@sonic.net>
2018-10-03 11:43:53 -0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,9 +2,14 @@ output "sec_group" {
   value = "${aws_security_group.ssh.id}"
 }
 
-output "instances" {
-  value = "${aws_instance.web.*.id}"
+output "private_instances" {
+  value = "${aws_instance.private_web.*.id}"
 }
+
+output "public_instances" {
+  value = "${aws_instance.public_web.*.id}"
+}
+
 
 output "private_hosts" {
   value = "${aws_instance.private_web.*.private_ip}"
